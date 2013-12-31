b2a8a05c7e0aeacc8efde64b9006d36cb73b9e6d
Sam Redmond <sam.redmond@menloschool.org>
2013-12-31 03:46:36 -0800

    Experiments with times JSON object for smart schedule lookup

--- app/views.py
+++ app/views.py
@@ -6,8 +6,17 @@ import json
 
 with open('app/topics.json') as f:
     topics = json.loads(f.read())
-# times = json.loads('times.json')
 
+#the day given by times refers to 12AM of the morning of the day of class
+with open('app/times.json') as f:
+    times = json.loads(f.read())
+
+if topics['num_classes'] != times['num_classes']:
+    raise Exception('Mismatch between number of topics and number of days')
+
+topics = topics['data']
+localtimes = [time.mktime(dt.strptime(t, "%m/%d/%y").timetuple()) for t in times['data']]
+#XXX Zip topics and times into one object
 
 @app.route('/')
 @app.route('/index')
@@ -20,3 +29,7 @@ def me():
     return render_template('loghw.html',
         title='Class Log and HW',
         db=topics)
+
+#Returns a class object if localtime is within one day of a classes' associated time, or None otherwise
+def getClassFromTime(localtime):
+    pass
